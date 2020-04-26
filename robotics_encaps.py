63ca15d800ff57efe8d409048013df6da8edf460
victorlisita <48096624+victorlisita@users.noreply.github.com>
2020-04-26 16:31:27 -0400

    Update robotics_encaps.py

--- robotics_encaps.py
+++ robotics_encaps.py
@@ -9,12 +9,39 @@ class Bin:
         part_id(str): shows id of a single Part object
         qty_in_bin(int): shows how many of said Part is in the bin
     """
-    def __init__(self, bin_id: int, location: str, part_id: str, qty_in_bin: int):
+    def __init__(self, bin_id: int, location: str, part_id: int, qty_in_bin: int):
         "Initializes description of the bin"
-        self.id = bin_id
-        self.location = location
-        self.part_id = part_id
-        self.qty_in_bin = qty_in_bin
+        self._id = bin_id
+        self._location = location
+        self._part_id = part_id
+        self._qty_in_bin = qty_in_bin
+
+    def get_id(self):
+        return self._id
+
+    def set_id(self, value: int):
+        assert value >= 0
+        self._id = value
+
+    def get_location(self):
+        return self._location
+
+    def set_location(self, value: str):
+        self._location = value
+
+    def get_part_id(self):
+        return self._part_id
+
+    def set_part_id(self, value: int):
+        assert value >= 0
+        self._part_id = value
+
+    def get_qty_in_bin(self):
+        return self._qty_in_bin
+
+    def set_qty_in_bin(self, value: int):
+        assert value >= 0
+        self._qty_in_bin = value
 
 
 class Part:
@@ -27,10 +54,37 @@ class Part:
     """
     def __init__(self, part_id: int, name: str, quantity: int, bin_id: int):
         "Initializes description of the part"
-        self.id = part_id
-        self.name = name
-        self.quantity = quantity
-        self.bin_id = bin_id
+        self._id = part_id
+        self._name = name
+        self._quantity = quantity
+        self._bin_id = bin_id
+
+    def get_id(self):
+        return self._id
+
+    def set_id(self, value: int):
+        assert value >= 0
+        self._id = value
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value: str):
+        self._name = value
+
+    def get_quantity(self):
+        return self._quantity
+
+    def set_quantity(self, value: int):
+        assert value >= 0
+        self._quantity = value
+
+    def get_bin_id(self):
+        return self._bin_id
+
+    def set_bin_id(self, value: int):
+        assert value >= 0
+        self._bin_id = value
 
 
 class User:
@@ -41,8 +95,20 @@ class User:
     """
     def __init__(self, email: str, student_num: int):
         "Initializes description of the student's information"
-        self.email = email
-        self.student_num = student_num
+        self._email = email
+        self._student_num = student_num
+
+    def get_email(self):
+        return self._email
+
+    def set_email(self, value: str):
+        self._email = value
+
+    def get_student_num(self):
+        return self._student_num
+
+    def set_student_num(self, value: str):
+        self._student_num = value
 
 
 class Log:
@@ -56,9 +122,30 @@ class Log:
     def __init__(self, time: datetime, user_id: int, part_id: int, quantity: int):
         "Initializes description of the log data"
         self.time = time
-        self.user_id = user_id
-        self.part_id = part_id
-        self.quantity = quantity
+        self._user_id = user_id
+        self._part_id = part_id
+        self._quantity = quantity
+
+    def get_user_id(self):
+        return self._user_id
+
+    def set_user_id(self, value: int):
+        assert value >= 0
+        self._user_id = value
+
+    def get_part_id(self):
+        return self._part_id
+
+    def set_part_id(self, value: int):
+        assert value >= 0
+        self._part_id = value
+
+    def get_quantity(self):
+        return self._quantity
+
+    def set_quantity(self, value: int):
+        assert value >= 0
+        self._quantity = value
 
 
 class InventoryManager:
